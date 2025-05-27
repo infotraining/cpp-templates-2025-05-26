a156df80520f99ac54ae1043dc1e31e0283fa128
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-05-27 13:19:18 +0200

    Enhance Stack class with push, pop, and top methods; add tests for stack operations and perfect forwarding in Container class

--- _exercises/ex-class-templates/ex_class_templates.cpp
+++ _exercises/ex-class-templates/ex_class_templates.cpp
@@ -12,130 +12,259 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include <set>
 
 #ifdef _MSC_VER
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
 
-// TODO: Stack
+template <typename TItem, typename TContainer = std::deque<TItem>>
+class Stack
+{
+private:
+    TContainer container{};
 
-// static_assert(std::is_same_v<Stack<int>::container_type, std::deque<int>>);
-// static_assert(std::is_same_v<Stack<int, std::vector<int>>::container_type, std::vector<int>>);
+public:
+    using container_type = TContainer;
+    using value_type = typename TContainer::value_type;
+    using reference = typename TContainer::reference;
 
-// // explicit template instantiation
+    std::size_t size() const
+    {
+        return container.size();
+    }
+    bool empty() const
+    {
+        return container.empty();
+    }
+
+    template <typename T>
+    void push(T&& _value)
+    {
+        container.push_back(std::forward<T>(_value));
+    }
+
+    void pop(value_type& _value)
+    {
+        _value = container.back();
+        container.pop_back();
+    }
+
+    reference top()
+    {
+        return container.back();
+    }
+};
+
+
+static_assert(std::is_same_v<Stack<int>::container_type, std::deque<int>>);
+static_assert(std::is_same_v<Stack<int, std::vector<int>>::container_type, std::vector<int>>);
+
+// explicit template instantiation
 // template class Stack<int>;
 // template class Stack<std::string>;
 
-// TEST_CASE("After construction", "[stack]")
-// {
-//     Stack<int> s;
+TEST_CASE("After construction", "[stack]")
+{
+    Stack<int> s;
 
-//     SECTION("is empty")
-//     {
-//         REQUIRE(s.empty());
-//     }
+    SECTION("is empty")
+    {
+        REQUIRE(s.empty());
+    }
 
-//     SECTION("size is zero")
-//     {
-//         REQUIRE(s.size() == 0);
-//     }
-// }
+    SECTION("size is zero")
+    {
+        REQUIRE(s.size() == 0);
+    }
+}
 
-// TEST_CASE("Pushing an item", "[stack,push]")
-// {
-//     Stack<int> s;
+TEST_CASE("Pushing an item", "[stack,push]")
+{
+    Stack<int> s;
 
-//     SECTION("is no longer empty")
-//     {
-//         s.push(1);
+    SECTION("is no longer empty")
+    {
+        s.push(1);
 
-//         REQUIRE(!s.empty());
-//     }
+        REQUIRE(!s.empty());
+    }
 
-//     SECTION("size is increased")
-//     {
-//         auto size_before = s.size();
+    SECTION("size is increased")
+    {
+        auto size_before = s.size();
 
-//         s.push(1);
+        s.push(1);
 
-//         REQUIRE(s.size() - size_before == 1);
-//     }
+        REQUIRE(s.size() - size_before == 1);
+    }
 
-//     SECTION("recently pushed item is on a top")
-//     {
-//         s.push(4);
+    SECTION("recently pushed item is on a top")
+    {
+        s.push(4);
 
-//         REQUIRE(s.top() == 4);
-//     }
-// }
+        REQUIRE(s.top() == 4);
+    }
+}
 
-// template <typename TStack>
-// std::vector<typename TStack::value_type> pop_all(TStack& s)
-// {
-//     std::vector<typename TStack::value_type> values(s.size());
+template <typename TStack>
+std::vector<typename TStack::value_type> pop_all(TStack& s)
+{
+    std::vector<typename TStack::value_type> values(s.size());
 
-//     for (auto& item : values)
-//         s.pop(item);
+    for (auto& item : values)
+        s.pop(item);
 
-//     return values;
-// }
+    return values;
+}
 
-// TEST_CASE("Pop all items")
-// {
-//     Stack<int> s;
+TEST_CASE("Pop all items")
+{
+    Stack<int> s;
 
-//     s.push(1);
-//     s.push(4);
+    s.push(1);
+    s.push(4);
 
-//     auto values = pop_all<Stack<int>>(s);
-//     REQUIRE(values.size() == 2);
-// }
+    auto values = pop_all<Stack<int>>(s);
+    REQUIRE(values.size() == 2);
+}
 
-// TEST_CASE("Popping an item", "[stack,pop]")
-// {
-//     Stack<int> s;
+TEST_CASE("Popping an item", "[stack,pop]")
+{
+    Stack<int> s;
 
-//     int x = 1;
-//     s.push(x);
-//     s.push(4);
+    int x = 1;
+    s.push(x);
+    s.push(4);
 
-//     int item;
+    int item;
 
-//     SECTION(" an item from a top to an argument passed by ref")
-//     {
-//         s.pop(item);
+    SECTION(" an item from a top to an argument passed by ref")
+    {
+        s.pop(item);
 
-//         REQUIRE(item == 4);
-//     }
+        REQUIRE(item == 4);
+    }
 
-//     SECTION("size is decreased")
-//     {
-//         size_t size_before = s.size();
+    SECTION("size is decreased")
+    {
+        size_t size_before = s.size();
 
-//         s.pop(item);
+        s.pop(item);
 
-//         REQUIRE(size_before - s.size() == 1);
-//     }
+        REQUIRE(size_before - s.size() == 1);
+    }
 
-//     SECTION("LIFO order")
-//     {
-//         int a, b;
+    SECTION("LIFO order")
+    {
+        int a, b;
 
-//         s.pop(a);
-//         s.pop(b);
+        s.pop(a);
+        s.pop(b);
 
-//         REQUIRE(a == 4);
-//         REQUIRE(b == 1);
-//     }
-// }
+        REQUIRE(a == 4);
+        REQUIRE(b == 1);
+    }
+}
 
-// TEST_CASE("Popping all items")
-// {
-//     Stack<int, std::vector<int>> s;
+TEST_CASE("Popping all items")
+{
+    Stack<int, std::vector<int>> s;
 
-//     s.push(1);
-//     s.push(4);
+    s.push(1);
+    s.push(4);
 
-//     auto values = pop_all(s);
-//     REQUIRE(values.size() == 2);
-// }
+    auto values = pop_all(s);
+    REQUIRE(values.size() == 2);
+}
+
+
+template <typename T>
+class IndexedSet : public std::set<T>
+{
+    using BaseType = std::set<T>;
+public:
+    using BaseType::BaseType; // inheritance of constructors
+
+    decltype(auto) operator[](size_t index)
+    {        
+        auto it = std::next(BaseType::begin(), index);
+        
+        return *it;
+    }
+};
+
+TEST_CASE("Inherit from std container")
+{
+    IndexedSet<int> my_set = {1, 2, 4, 5, 6, 7};
+    my_set.insert(3);
+
+    REQUIRE(my_set.find(5) != my_set.end());
+
+    REQUIRE(my_set[2] == 3);
+}
+
+/////////////////////////////////////////////
+
+struct ShapeBase
+{
+    virtual void draw() const = 0;
+    virtual ~ShapeBase() = default;    
+};
+
+struct Circle : public ShapeBase
+{
+    int radius;
+
+    explicit Circle(int r) : radius{r}
+    {}
+
+    ~Circle()
+    {
+        std::cout << "~Circle()\n";
+    }
+
+    void draw() const override
+    {
+        std::cout << "Drawing circle r=" << radius << "...\n";
+    }
+};
+
+struct ColorCircle : Circle
+{
+    using Circle::Circle;
+
+    void draw() const override
+    {
+        std::cout << "Drawing color circle r=" << radius << "...\n";
+    }
+
+    ~ColorCircle()
+    {
+        std::cout << "~ColorCircle()\n";
+    }
+};
+
+TEST_CASE("virtual destructors")
+{
+    // {
+    //     std::unique_ptr<ShapeBase> ptr_shape = std::make_unique<ShapeBase>();
+    //     ptr_shape->draw();
+
+    //     ptr_shape = std::make_unique<ColorCircle>(100);
+    //     ptr_shape->draw();
+    // }
+    
+    std::vector<std::unique_ptr<ShapeBase>> shapes;
+
+    shapes.push_back(std::make_unique<ColorCircle>(100));
+    shapes.push_back(std::make_unique<Circle>(160));
+    shapes.push_back(std::make_unique<ColorCircle>(100));
+    shapes.push_back(std::make_unique<Circle>(160));
+    shapes.push_back(std::make_unique<ColorCircle>(100));
+    shapes.push_back(std::make_unique<Circle>(160));
+
+    for(const auto& ptr_s : shapes)
+        ptr_s->draw();    
+}
+
--- type-deduction/type_deduction.cpp
+++ type-deduction/type_deduction.cpp
@@ -22,14 +22,14 @@ void deduce1(T arg)
 namespace Cpp20
 {
     void deduce1(auto arg)
-{
-    std::cout << __PRETTY_FUNCTION__ << "\n";
-}
-}
+    {
+        std::cout << __PRETTY_FUNCTION__ << "\n";
+    }
+} // namespace Cpp20
 
 template <typename T>
 void deduce2(T& arg)
-{    
+{
     std::cout << __PRETTY_FUNCTION__ << "\n";
 }
 
@@ -39,15 +39,69 @@ void deduce3(T&& arg)
     std::cout << __PRETTY_FUNCTION__ << "\n";
 }
 
+namespace Cpp20
+{
+    void deduce3(auto&& arg)
+    {
+    }
+} // namespace Cpp20
+
+struct Container
+{
+    std::vector<std::string> items;
+
+    // void add(const std::string& item)
+    // {
+    //     items.push_back(item); // insert copy of item
+    // }
+
+    // void add(std::string&& item)
+    // {
+    //     items.push_back(std::move(item));
+    // }
+
+    // perfect forwarding
+    template <typename TItem>
+    void add(TItem&& item) // universal reference
+    {
+        items.push_back(std::forward<TItem>(item));
+    }
+};
+
+namespace Explain
+{
+    template <typename T>
+    decltype(auto) forward(T&& arg)
+    {
+        if constexpr (std::is_lvalue_reference_v<T>)
+        {
+            return arg;
+        }
+        else
+        {
+            return std::move(arg);
+        }
+    }
+} // namespace Explain
+
+TEST_CASE("perfect forwarding")
+{
+    Container container;
+
+    std::string str = "text";
+    container.add(str); // lvalue
+
+    container.add(std::string("abc"));
+}
+
 // REFERENCE COLLAPSING
 // & &   -> &  // C++98
 // && &  -> &
 // & &&  -> &
 // && && -> &&
 
-
 void foo(int)
-{}
+{ }
 
 namespace Explain
 {
@@ -62,7 +116,7 @@ namespace Explain
     {
         return N;
     }
-}
+} // namespace Explain
 
 TEST_CASE("size")
 {
@@ -72,7 +126,6 @@ TEST_CASE("size")
     int native_tab[10] = {1, 2, 3};
     static_assert(Explain::size(native_tab) == 10);
 }
-
 
 TEST_CASE("type deduction rules")
 {
@@ -84,48 +137,48 @@ TEST_CASE("type deduction rules")
     static_assert(sizeof(tab) == 10 * sizeof(int));
 
     SECTION("Case 1")
-    {        
-        deduce1(x);      // deduce1<int>(int) 
+    {
+        deduce1(x);      // deduce1<int>(int)
         deduce1(cx);     // deduce1<int>(int)
         deduce1(ref_x);  // deduce1<int>(int)
         deduce1(cref_x); // deduce1<int>(int)
         deduce1(tab);    // deduce1<int*>(int*)  - decays to pointer
         deduce1("text"); // deduce1<const char*>(const char*)
-        deduce1(foo);    // deduce1<void(*)(int)>(void(*)(int))  
+        deduce1(foo);    // deduce1<void(*)(int)>(void(*)(int))
 
         auto a1 = x;      // int
         auto a2 = cx;     // int
-        auto a3 = ref_x;  // int 
-        auto a4 = cref_x; // int 
+        auto a3 = ref_x;  // int
+        auto a4 = cref_x; // int
         auto a5 = tab;    // int*  - array decays to pointer
         auto a6 = foo;    // void(*a6)(int) - function decays to pointer
     }
 
     SECTION("Case 2")
     {
-        deduce2(x);      // deduce2<int>(int&)     
-        deduce2(cx);     // deduce2<const int>(const int&)  
-        deduce2(ref_x);  // deduce2<int>(int&)  
-        deduce2(cref_x); // deduce2<const int>(const int&)   
+        deduce2(x);      // deduce2<int>(int&)
+        deduce2(cx);     // deduce2<const int>(const int&)
+        deduce2(ref_x);  // deduce2<int>(int&)
+        deduce2(cref_x); // deduce2<const int>(const int&)
         deduce2(tab);    // deduce2<int[10]>(int(&)[10])
         deduce2(foo);    // deduce2<void(int)>(void(&)(int))
 
-        auto& a1 = x;       // int&
-        auto& a2 = cx;      // const int&
-        auto& a3 = ref_x;   // int&
-        auto& a4 = cref_x;  // const int&
-        auto& a5 = tab;     // int (&a5)[10] = tab;
-        auto& a6 = foo;     // void (&a6)(int) = foo;
+        auto& a1 = x;      // int&
+        auto& a2 = cx;     // const int&
+        auto& a3 = ref_x;  // int&
+        auto& a4 = cref_x; // const int&
+        auto& a5 = tab;    // int (&a5)[10] = tab;
+        auto& a6 = foo;    // void (&a6)(int) = foo;
     }
 
     SECTION("Case 3")
     {
-        deduce3(x);  // x - lvalue: deduce3<int&>(int&)            
-        deduce3(cx); // cx - lvalue: deduce3<const int&>(const int&)            
-        deduce3(ref_x);  // ref_x - lvalue: deduce3<int&>(int&)
-        deduce3(cref_x); // cref_x - lvalue: deduce3<const int&>(const int&)         
-        deduce3(tab);    // tab - lvalue: deduce3<int(&)[10]>(int(&)[10])            
-        deduce3(foo);    // foo - lvalue: deduce3<void(&)(int)>(void(&)(int))        
+        deduce3(x);              // x - lvalue: deduce3<int&>(int&)
+        deduce3(cx);             // cx - lvalue: deduce3<const int&>(const int&)
+        deduce3(ref_x);          // ref_x - lvalue: deduce3<int&>(int&)
+        deduce3(cref_x);         // cref_x - lvalue: deduce3<const int&>(const int&)
+        deduce3(tab);            // tab - lvalue: deduce3<int(&)[10]>(int(&)[10])
+        deduce3(foo);            // foo - lvalue: deduce3<void(&)(int)>(void(&)(int))
         deduce3(string("text")); // string("text") - rvalue: deduce3<std::string>(std::string&&)
 
         auto&& a1 = x;
@@ -138,6 +191,66 @@ TEST_CASE("type deduction rules")
     }
 }
 
+template <typename T>
+auto multiply(T a, T b)
+{
+    return a * b;
+}
+
 TEST_CASE("decltype(auto)")
-{ 
+{
+    int x = 42;
+    const std::unordered_map<std::string, int> dict = { {"one", 1}, {"two", 2} };
+
+    auto ax1 = x;
+    auto ax2 = dict;
+
+    REQUIRE(ax2.size() == 2);
+
+    decltype(x) d1{};
+    decltype(dict) d2;
+}
+
+template <typename TContainer>
+decltype(auto) get_nth(TContainer& container, size_t nth)
+{
+    std::remove_reference_t<decltype(container)> other_container;
+
+    return container[nth]; 
+}
+
+constexpr int foobar()
+{
+    int* ptr = new int[10];
+
+    ptr[1] = 2;
+
+    delete[] ptr;
+    return 53;
+}
+
+TEST_CASE("foobar")
+{
+    constexpr int x = foobar();
+}
+
+TEST_CASE("auto vs. decltype(auto)")
+{
+    std::vector<int> vec = {1, 2, 3};
+
+    auto result = get_nth(vec, 1);
+    REQUIRE(result == 2);
+
+    get_nth(vec, 1) = 665;
+    REQUIRE(vec[1] == 665);
+
+    std::vector<std::string> words = { "one", "two" };
+
+    get_nth(words, 0) = "new";
+    REQUIRE(words[0] == "new");
+
+    std::vector<bool> vec_bool = {0, 1, 1, 1};
+
+    get_nth(vec_bool, 1) = 0;
+    REQUIRE(vec_bool[1] == 0);
 }
